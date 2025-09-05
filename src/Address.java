49a8ea183233b9f0eab47328cf993e2ecae21105
Edson Benedito da Silva <edsonbe.silva@outlook.com>
2025-09-04 21:42:41 -0300

    searching cep

---
+++ src/Address.java
@@ -0,0 +1,3 @@
+public record Address(String logradouro, String cep, int ddd,
+                      String localidade, String bairro, String uf, String complemento) {
+}
---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner reading = new Scanner(System.in);
+        CheckCep checkCep = new CheckCep();
+
+        System.out.println("Digite um numero de cep para consulta");
+        var cep = reading.nextLine();
+
+       try {
+           Address newAddress = checkCep.searchAddress(cep);
+           System.out.println(newAddress);
+           FileGenerator gerator = new FileGenerator();
+           gerator.saveJson(newAddress);
+       } catch (RuntimeException | IOException e) {
+           System.out.println(e.getMessage());
+           System.out.println("Finalizando aplicação");
+       }
+
+
+
+    }
+}
